844962fca71d08170f3f2424ae78f07ca1ce3c90
Leo Hu <lalalaleo.hu@hotmail.com>
2017-05-29 19:50:38 +0800

    登录注册基本完成
    除数据交换部分

--- app/src/main/java/zucc/tm/jg/View/LoginFragment.java
+++ app/src/main/java/zucc/tm/jg/View/LoginFragment.java
@@ -13,15 +13,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 import zucc.tm.jg.R;
 import zucc.tm.jg.Util.NoScrollListview;
+import zucc.tm.jg.Util.alertdialog;
 import zucc.tm.jg.adapter.rwAdapter;
 
 public class LoginFragment extends Fragment {
-    private Toolbar toolbar;
+    private EditText et_call;
+    private EditText et_passwd;
+    private Button btn_login;
     public static Fragment newInstance(){
         LoginFragment fragment = new LoginFragment();
         return fragment;
@@ -30,6 +35,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login,null);
+        initView(view);
         return view;
     }
 
@@ -41,5 +47,28 @@ public class LoginFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    public void initView(View view){
+        et_call = (EditText) view.findViewById(R.id.et_call);
+        et_passwd = (EditText) view.findViewById(R.id.et_passwd);
+        btn_login = (Button) view.findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String call = et_call.getText().toString();
+                String passwd = et_passwd.getText().toString();
+                if(call.equals("")){
+                    alertdialog.showSimpleDialog(getActivity(), "提醒", "手机号不能为空", "确定", null, null,null, true);
+                }
+                else if(passwd.equals("")){
+                    alertdialog.showSimpleDialog(getActivity(), "提醒", "密码不能为空", "确定", null, null,null, true);
+                }
+                else {
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/zucc/tm/jg/View/RegisterFragment.java
+++ app/src/main/java/zucc/tm/jg/View/RegisterFragment.java
@@ -1,24 +1,28 @@
 package zucc.tm.jg.View;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.net.Uri;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import zucc.tm.jg.R;
+import zucc.tm.jg.Util.PhoneFormatCheckUtils;
+import zucc.tm.jg.Util.Projectlistb;
+import zucc.tm.jg.Util.alertdialog;
+
+import static zucc.tm.jg.Util.my.my;
 
 public class RegisterFragment extends Fragment {
-    private Toolbar toolbar;
-    private Button btn_login;
-    private ViewPager mViewPager;
+    private EditText et_call;
+    private EditText et_passwd;
+    private EditText et_repasswd;
+    private Button btn_register;
     public static Fragment newInstance(){
         RegisterFragment fragment = new RegisterFragment();
         return fragment;
@@ -42,6 +46,30 @@ public class RegisterFragment extends Fragment {
     }
 
     public void initView(View view){
+        et_call = (EditText) view.findViewById(R.id.et_call);
+        et_passwd = (EditText) view.findViewById(R.id.et_passwd);
+        et_repasswd = (EditText) view.findViewById(R.id.et_repasswd);
+        btn_register = (Button) view.findViewById(R.id.btn_register);
 
+        btn_register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String call = et_call.getText().toString();
+                String passwd = et_passwd.getText().toString();
+                String repasswd = et_repasswd.getText().toString();
+                if(call.equals("")){
+                    alertdialog.showSimpleDialog(getActivity(), "提醒", "手机号不能为空", "确定", null, null,null, true);
+                }
+                else if(passwd.equals("")){
+                    alertdialog.showSimpleDialog(getActivity(), "提醒", "密码不能为空", "确定", null, null,null, true);
+                }
+                else if(!passwd.equals(repasswd)){
+                    alertdialog.showSimpleDialog(getActivity(), "提醒", "两次密码输入不一致", "确定", null, null,null, true);
+                }
+                else {
+
+                }
+            }
+        });
     }
 }
