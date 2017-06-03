7034b42ccd39baa8073e53565a8604f3df1b1810
Leo Hu <lalalaleo.hu@hotmail.com>
2017-06-03 18:37:03 +0800

    引导页面设计完成

---
+++ app/src/main/java/zucc/tm/jg/View/Loading.java
@@ -0,0 +1,13 @@
+package zucc.tm.jg.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
