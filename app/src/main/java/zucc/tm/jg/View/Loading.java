3c6e070315525c03ea58175b26fc7e70a69fb7cb
Leo Hu <lalalaleo.hu@hotmail.com>
2017-06-03 18:51:01 +0800

    引导页面完成

--- app/src/main/java/zucc/tm/jg/View/Loading.java
+++ app/src/main/java/zucc/tm/jg/View/Loading.java
@@ -1,7 +1,13 @@
 package zucc.tm.jg.View;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import zucc.tm.jg.R;
 
 public class Loading extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class Loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        final Intent intent = new Intent(this,LoginActivity.class);
+        Timer timer = new Timer();
+        TimerTask tast = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        };
+        timer.schedule(tast, 3000);
     }
 }
